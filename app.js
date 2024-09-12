150c80528991c030061ebb3c61c277b1ba2e587c
Subodh Vashistha <subodh.vashistha@oracle.com>
2024-09-12 23:48:16 +0530

    Updated content in authwithReact submodule

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("./db/userModel");
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -19,3 +21,43 @@ const dbConnect = require("./db/dbConnect");
 
 // execute database connection 
 dbConnect();
+
+
+// register endpoint
+app.post("/register", (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
